fdd590d9be835401bd224ded2112b886cb61983d
Asmat <asmat9630@gmail.com>
2026-05-14 18:46:29 +0500

    ci/cd pipline added

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('CI/CD Pipeline Working Successfully 🚀');
+  res.send('CI/CD Pipeline Working  and updated Successfully 🚀');
 });
 
 app.listen(3000, () => {
